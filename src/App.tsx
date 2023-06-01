00d98dceb77d597763d97d9ceddbcd476de9b236
Nikkhil420 <nikhilbujade419@gmail.com>
2023-06-01 17:46:56 +0530

    main : Task-3 / 1/06

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { resolve } from "path";
+import { rejects } from "assert";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [Username, setUsername] = useState<string>("N");
+ const [UserAge, setUserAge] = useState<number>(25);
+ const [UserCity, setUserCity] = useState<string>("Nagpur");
+
+
+  const UsernameApi = () => {
+    return new Promise<any>((resolve, reject) => {
+      return setTimeout(() => {
+        resolve({ name: " Nikhil " });
+      }, 1000);
+    });
+  };
+  const UserAgeApi = () => {
+    return new Promise<any>((resolve, reject) => {
+      return setTimeout(() => {
+        resolve({ age: 20 });
+      }, 1500);
+    });
+  };
+  const UserCityApi = () => {
+    return new Promise<any>((resolve, reject) => {
+      return setTimeout(() => {
+        resolve({ city: " Mumbai " });
+      }, 1500);
+    });
+  };
+
+
+
+  const getUsername = async () => {
+    
+    const username = await UsernameApi();
+    console.log("username", username);
+    const { name } = username;
+     setUsername(name);
+  };
+
+  const getUserAge = async () => {
+    
+    const UserAge = await UserAgeApi();
+    console.log("UserAge", UserAge);
+    const { age} = UserAge;
+     setUserAge(age);
+  };
+
+  const getUserCity = async () => {
+    
+    const UserCity = await UserCityApi();
+    console.log("UserCity", UserCity);
+    const { city } = UserCity;
+     setUserCity(city);
+  };
+
+
+  useEffect(() => {
+    getUsername()
+  }, []);
+
+  useEffect(() => {
+    getUserAge();
+  }, []);
+
+  useEffect(() => {
+    getUserCity();
+  }, []);
+
+
+  return <div>
+
+ <h1>Username = {Username}</h1>
+ <h1>Age = {UserAge}</h1>
+ <h1>City = {UserCity}</h1>
+
+
+  </div>;
 }
 
 export default App;
